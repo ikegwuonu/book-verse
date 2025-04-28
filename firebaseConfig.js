b11240d55082ed83b7d39a4edc75216e6e6f28ef
ikegwuonu <juliet.kegwuonu@gmail.com>
2025-04-28 01:34:25 +0100

    ignore build err

---
+++ firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBnLWR0-9pty_RsexT6VSc117KirX3pO_A",
+  authDomain: "book-verse-a8fc1.firebaseapp.com",
+  projectId: "book-verse-a8fc1",
+  storageBucket: "book-verse-a8fc1.firebasestorage.app",
+  messagingSenderId: "434240705091",
+  appId: "1:434240705091:web:d1c73531c8eb16d37abaf6",
+  measurementId: "G-4YSZC9M8SE",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
